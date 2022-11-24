6885ae05b45e56300ae012dc7dfe3d83d7dc2cda
drozboril <rozborildavid77@gmail.com>
2022-11-24 17:34:06 +0100

    Uživatel zadá hranice intervalu, pak zadá číslo a program vypíše jestli se dané číslo nachází v intervalu

---
+++ Interval/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadejte počáteční bod intervalu");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Zadejte konečný bod intervalu");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Zadejte číslo");
+            int c = int.Parse(Console.ReadLine());
+            if (a > b)
+            {
+                a = a + b;
+                b = a - b;
+                a = a - b;
+            }
+            if (a <= c && c >= b) //c musí být rovno nebo větší než a ale zároveň musí být rovno nebo menší než b
+            {
+                Console.WriteLine("Číslo {0} nespadá do intervalu čísel {1} a {2}", c, a, b);
+            }
+            else
+            {
+                Console.WriteLine("Číslo {0} spadá do intervalu čísel {1} a {2}", c, a, b);
+            }
+            Console.ReadLine();
+        }
+    }
+}
